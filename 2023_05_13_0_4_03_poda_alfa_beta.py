05d89b710fed86ce5da68ac4fc1a1f9cbe8bbe41
Alnitakr51 <gadieljj34@gmail.com>
2023-05-14 02:05:50 +0200

    Juegos

---
+++ 2023_05_13_0_4_03_poda_alfa_beta.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 13 23:40:24 2023
+
+@author: Gadiel Jimenez
+"""
+
+poda
---
+++ 2023_05_13_0_4_06_Posibilidad_minimax_esperado.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 13 23:40:25 2023
+
+@author: Gadiel Jimenez
+"""
+pomimax
